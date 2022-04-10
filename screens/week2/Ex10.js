fd6657e73487511e8851f6d2ef41ec64ea8fbf79
anirut3198705 <95035200+anirut3198705@users.noreply.github.com>
2022-04-10 17:55:59 +0700

    week 2

---
+++ screens/week2/Ex10.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Ex10() {
+    return (
+        <View style={{ flex : 1 , flexDirection : 'column' }}>    
+        <View style={{ backgroundColor : '#4A90E2' , height : 1000  }}></View>      
+      </View>
+    );
+}
